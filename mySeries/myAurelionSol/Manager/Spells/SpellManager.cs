7466ee1690b1a9eeb354d60e515394c927caf811
NightMoon032 <407368027@qq.com>
2016-12-02 16:12:30 +0800

    Update AurelionSol

--- mySeries/myAurelionSol/Manager/Spells/SpellManager.cs
+++ mySeries/myAurelionSol/Manager/Spells/SpellManager.cs
@@ -7,10 +7,10 @@
     {
         internal static void Init()
         {
-            Q = new Spell(SpellSlot.Q, 700f);
-            W = new Spell(SpellSlot.W, 675f);
-            E = new Spell(SpellSlot.E, 400f);
-            R = new Spell(SpellSlot.R, 1550f);
+            Q = new Spell(SpellSlot.Q, 700f, TargetSelector.DamageType.Magical);
+            W = new Spell(SpellSlot.W, 675f, TargetSelector.DamageType.Magical);
+            E = new Spell(SpellSlot.E, 400f, TargetSelector.DamageType.Magical);
+            R = new Spell(SpellSlot.R, 1550f, TargetSelector.DamageType.Magical);
 
             Q.SetSkillshot(0.40f, 180, 800, false, SkillshotType.SkillshotCircle);
             R.SetSkillshot(0.25f, 180, 1750, false, SkillshotType.SkillshotLine);
